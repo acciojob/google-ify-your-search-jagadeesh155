077a6b3bdd72ed4c23be182e334fe38042a61900
jagadeesh155 <111222092+jagadeesh155@users.noreply.github.com>
2025-08-26 15:11:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Google Search Button
+document.getElementById("searchBtn").addEventListener("click", function() {
+  alert("Google Search clicked!");
+});
+
+// I'm Feeling Lucky Button
+document.getElementById("luckyBtn").addEventListener("click", function() {
+  alert("I'm Feeling Lucky clicked!");
+});
